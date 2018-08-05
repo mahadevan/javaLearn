18b88d5ac4f26682eca85435087721f02b682788
Mahadevan Iyer <mahadevan.n.iyer@gmail.com>
2018-08-05 16:33:44 +0530

    New Changes to Git via VSCODE

--- index.java
+++ index.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class index{
     public static void main(String[] args) {
-        System.out.println("New");
+        System.out.println("New Changes");
         userChoice();
     }
 
